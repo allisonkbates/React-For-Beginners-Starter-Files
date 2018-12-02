5461a575ccb38524332db858e52677ddf6d321ce
Allison Bates <allisonkbates@gmail.com>
2018-12-01 19:48:59 -0500

    video 13, 13 minutes in

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -8,11 +8,10 @@ class StorePicker extends React.Component {
 		// 1. stop the form from submitting
 		event.preventDefault();
 		// 2. get the text from that input
-		console.log(this);
+		const storeName = this.myInput.value.value;
 		// 3. change the page to /store/whateverrrrr
+		this.props.history.push(`/store/${storeName}`);
 	};
-
-
 	render() {
 		return ( 
 			<form className="store-selector" onSubmit={this.goToStore}>

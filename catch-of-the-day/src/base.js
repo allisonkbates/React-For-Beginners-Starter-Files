531cc51821fee9836e0fddb12d38ae0d8d370796
Allison Bates <allisonkbates@gmail.com>
2019-01-24 20:17:43 -0500

    video 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,16 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCEXLq8S5kwewD5i-ElRH79NuPoX4FV5uM",
+    authDomain: "alli-b-catch-of-the-day.firebaseapp.com",
+    databaseURL: "https://alli-b-catch-of-the-day.firebaseio.com",
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+//this is a named export
+export { firebaseApp };
+
+//this is a default export
+export default base;

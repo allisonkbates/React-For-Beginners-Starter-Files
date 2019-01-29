ebd362c0206e51c594770201b2e5a44364e259f8
Allison Bates <allisonkbates@gmail.com>
2019-01-28 19:07:13 -0500

    video 21

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -49,6 +49,16 @@ class App extends React.Component {
 		this.setState({ fishes });
 	}
 
+	deleteFish = (key) => {
+		//take a copy of state
+		const fishes = {...this.state.fishes};
+		//update teh state
+		fishes[key] = null;
+		//update state
+		this.setState({ fishes });
+
+	}
+
 	loadSampleFishes = () => {
 		this.setState({ fishes: sampleFishes });
 	};
@@ -62,6 +72,12 @@ class App extends React.Component {
 		this.setState({ order });
 	}
 
+	removeFromOrder = (key) => {
+		const order = {...this.state.order};
+		delete order[key];
+		this.setState({ order });
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -69,14 +85,19 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market"/>
 					<ul className="fishes">
 						{Object.keys(this.state.fishes).map(key => (
-							<Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
+							<Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
 							))}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order}/>
+				<Order 
+					fishes={this.state.fishes} 
+					order={this.state.order} 
+					removeFromOrder={this.removeFromOrder}
+				/>
 				<Inventory 
 					addFish={this.addFish} 
 					updateFish={this.updateFish} 
+					deleteFish={this.deleteFish}
 					loadSampleFishes={this.loadSampleFishes}
 					fishes={this.state.fishes}
 				/>
